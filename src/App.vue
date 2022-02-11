82f1d2af36f25596569d031ecceb690de5232b25
tuzki <11761740@qq.com>
2022-02-11 17:54:14 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: tuzki zhang
+ * @Date: 2022-02-11 17:16:27
+ * @LastEditors: tuzki zhang
+ * @LastEditTime: 2022-02-11 17:29:47
+ * @Description: file content
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
